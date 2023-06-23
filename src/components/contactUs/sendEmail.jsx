45f8ea0af36a127ba7b197131820c94ff3ddfa56
David <dmg_92@hotmail.com>
2023-06-23 10:47:11 -0600

    form

--- src/components/contactUs/sendEmail.jsx
+++ src/components/contactUs/sendEmail.jsx
@@ -1,27 +1,20 @@
 import { Col, Row, Container, Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
-//import "./contactus.css";
+import "./sendEmail.css";
 import emailjs from "emailjs-com";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { MyModal } from "../modal/modal";
 
-
-
-
-
-
 export const SendEmail = () => {
-
   const [showModal, setShowModal] = useState(false);
-  const [message, setMessage] = useState('');
-
+  const [message, setMessage] = useState("");
 
   function sendEmail(e) {
     const emailjs_service_id = process.env.REACT_APP_EMAILJS_SERVICE_ID;
     const emailjs_template_id = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
     const emailjs_user_key = process.env.REACT_APP_EMAILJS_USER_kEY;
 
-    console.log('emailjs_service_id', emailjs_service_id)
+    console.log("emailjs_service_id", emailjs_service_id);
     e.preventDefault();
 
     emailjs
@@ -34,59 +27,55 @@ export const SendEmail = () => {
       .then(
         (result) => {
           console.log("Email successfully sent!");
-          setMessage('El formulario se envió correctamente.');
+          setMessage("El formulario se envió correctamente.");
           setShowModal(true);
         },
         (error) => {
           console.error("Error sending email:", error);
-          setMessage('Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
-      setShowModal(true);
-
+          setMessage(
+            "Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo más tarde."
+          );
+          setShowModal(true);
         }
       );
   }
 
   return (
-    <section >
-            <Form onSubmit={sendEmail}>
-              <Form.Group className="mb-3" controlId="formName">
-                <Form.Label>Nombre completo</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Nombre completo"
-                  name="name"
-                />
-              </Form.Group>
+    <section>
+      <Form onSubmit={sendEmail}>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>Nombre completo</Form.Label>
+          <Form.Control type="text" placeholder="Nombre completo" name="name" />
+        </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Correo electrónico</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Correo electrónico"
-                  name="email"
-                />
-              </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Correo electrónico</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Correo electrónico"
+            name="email"
+          />
+        </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formPhone">
-                <Form.Label>Teléfono</Form.Label>
-                <Form.Control type="text" placeholder="Teléfono" name="phone" />
-              </Form.Group>
+        <Form.Group className="mb-3" controlId="formPhone">
+          <Form.Label>Teléfono</Form.Label>
+          <Form.Control type="text" placeholder="Teléfono" name="phone" />
+        </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formCompany">
-                <Form.Label>Empresa</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Empresa"
-                  name="company"
-                />
-              </Form.Group>
+        <Form.Group className="mb-3" controlId="formCompany">
+          <Form.Label>Empresa</Form.Label>
+          <Form.Control type="text" placeholder="Empresa" name="company" />
+        </Form.Group>
 
-              <Button variant="primary" type="submit">
-                Enviar
-              </Button>
-            </Form>
-            <MyModal showModal={showModal} setShowModal={setShowModal} message={message}/>
-
+        <Button variant="primary" type="submit">
+          Enviar
+        </Button>
+      </Form>
+      <MyModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        message={message}
+      />
     </section>
   );
 };
